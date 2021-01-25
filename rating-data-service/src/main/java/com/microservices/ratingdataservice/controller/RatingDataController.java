e1a731c513b2f9edbb11c0bf43d3ffa1d4954be7
rohan <iamrohanjainn@gmail.com>
2021-01-25 17:28:10 +0530

    level 1 microservice communication

---
+++ rating-data-service/src/main/java/com/microservices/ratingdataservice/controller/RatingDataController.java
@@ -0,0 +1,37 @@
+package com.microservices.ratingdataservice.controller;
+
+import com.microservices.ratingdataservice.model.Rating;
+import com.microservices.ratingdataservice.model.UserRating;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequestMapping("/ratingsdata")
+@RestController
+public class RatingDataController {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+
+        return  new Rating(4,movieId);
+
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId){
+
+        List<Rating> ratings=Arrays.asList(
+          new Rating(4,"123"),
+        new Rating(4,"456")
+        );
+        UserRating userRating=new UserRating();
+        userRating.setUserRating(ratings);
+        return  userRating;
+    }
+
+}
---
+++ rating-data-service/src/main/java/com/microservices/ratingdataservice/model/Rating.java
@@ -0,0 +1,28 @@
+package com.microservices.ratingdataservice.model;
+
+public class Rating {
+
+    private int rating;
+    private String movieId;
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public Rating(int rating, String movieId) {
+        this.rating = rating;
+        this.movieId = movieId;
+    }
+}
